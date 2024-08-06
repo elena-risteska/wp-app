f1f3828d3e105dc99d2ca05ac4a440e9c5abc9b1
elena-risteska <elena.risteska003@gmail.com>
2024-08-06 16:49:01 +0200

    update password field with show/hide feature

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,24 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+import authentication from "./authentication.svg";
+
 export default function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  function handlePassword() {
+    setShowPassword(!showPassword);
+  }
   return (
     <>
       <div className="font-[sans-serif] bg-stone-300 md:h-screen">
         <div className="grid md:grid-cols-2 items-center gap-8 h-full">
           <div className="max-md:order-1 p-4">
-            <img
-              src="https://readymadeui.com/signin-image.webp"
-              className="lg:max-w-[80%] w-full h-full object-contain block mx-auto"
-              alt="login-image"
+            <Image
+              className="ml-32"
+              src={authentication}
+              alt="authentication-image"
             />
           </div>
 
@@ -79,23 +90,25 @@ export default function LoginPage() {
                 <div className="relative flex items-center">
                   <input
                     name="password"
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     required
                     className="w-full text-sm border-b border-gray-300 focus:border-gray-800 px-2 py-3 outline-none"
                     placeholder="Enter password"
                   />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#bbb"
-                    stroke="#bbb"
-                    className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
-                    viewBox="0 0 128 128"
-                  >
-                    <path
-                      d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                      data-original="#000000"
-                    ></path>
-                  </svg>
+                  <button type="button" onClick={handlePassword}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#bbb"
+                      stroke="#bbb"
+                      className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+                      viewBox="0 0 128 128"
+                    >
+                      <path
+                        d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                        data-original="#000000"
+                      ></path>
+                    </svg>
+                  </button>
                 </div>
               </div>
 
@@ -116,7 +129,7 @@ export default function LoginPage() {
                 </div>
                 <div>
                   <a
-                    href=""
+                    href="forgot-password"
                     className="text-stone-800 font-semibold text-sm hover:underline"
                   >
                     Forgot Password?
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,12 @@
+"use client";
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  function handlePassword() {
+    setShowPassword(!showPassword);
+  }
   return (
     <>
       <div className="font-[sans-serif] bg-stone-300 md:h-screen">
@@ -19,7 +27,10 @@ export default function RegisterPage() {
                 </a>
               </p>
             </div>
-            <form className="max-w-lg w-full mx-auto grid grid-cols-2">
+            <form
+              className="max-w-lg w-full mx-auto grid grid-cols-2"
+              action="#"
+            >
               <div className="m-3">
                 <label className="text-stone-950 text-xs block mb-2 ">
                   First name
@@ -200,23 +211,25 @@ export default function RegisterPage() {
                 <div className="relative flex items-center">
                   <input
                     name="password"
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     required
                     className="w-full text-sm border-b border-gray-300 focus:border-gray-800 px-2 py-3 outline-none"
                     placeholder="Enter password"
                   />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#bbb"
-                    stroke="#bbb"
-                    className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
-                    viewBox="0 0 128 128"
-                  >
-                    <path
-                      d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                      data-original="#000000"
-                    ></path>
-                  </svg>
+                  <button type="button" onClick={handlePassword}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#bbb"
+                      stroke="#bbb"
+                      className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+                      viewBox="0 0 128 128"
+                    >
+                      <path
+                        d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                        data-original="#000000"
+                      ></path>
+                    </svg>
+                  </button>
                 </div>
               </div>
 
@@ -227,23 +240,25 @@ export default function RegisterPage() {
                 <div className="relative flex items-center">
                   <input
                     name="password"
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     required
                     className="w-full text-sm border-b border-gray-300 focus:border-gray-800 px-2 py-3 outline-none"
                     placeholder="Confirm password"
                   />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#bbb"
-                    stroke="#bbb"
-                    className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
-                    viewBox="0 0 128 128"
-                  >
-                    <path
-                      d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                      data-original="#000000"
-                    ></path>
-                  </svg>
+                  <button type="submit" onClick={handlePassword}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#bbb"
+                      stroke="#bbb"
+                      className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+                      viewBox="0 0 128 128"
+                    >
+                      <path
+                        d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                        data-original="#000000"
+                      ></path>
+                    </svg>
+                  </button>
                 </div>
               </div>
 
